37dea7bc06e18e422d7a6b2734ec715cf55c6d9e
rpatew2c <104742612+rpatew2c@users.noreply.github.com>
2024-09-23 12:01:16 -0400

    Create CacheInterface.php

---
+++ CacheInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+interface CacheInterface
+{
+    /**
+     * @param string $key
+     * @param int|string|float|array|null $value
+     * @param int $duration Duration in seconds
+     */
+    public function set(string $key, $value, int $duration): void
+
+    /**
+     * @param string $key
+     * @return int|string|float|array|null
+     */
+    public function get(string $key);
+}
